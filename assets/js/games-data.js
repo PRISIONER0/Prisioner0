e24f5992408629648a39fe9872cdd8886a862a86
PRISIONER0_ <javiieergutierrez01@gmail.com>
2026-09-13 16:11:30 -0300

    Update games-data.js

--- assets/js/games-data.js
+++ assets/js/games-data.js
@@ -76,7 +76,7 @@ const games = [
         android: true,
     },
     {
-        title: "reservedtable",
+        title: "Reservedtable",
         page: "reservedtable.html",
         cover: "reservedtable.webp",
         featured: false,
